53551f432b27e0a9e4e86993fe70d675571fa44c
Johanna <hupana@hotmai.fi>
2021-01-13 01:40:01 +0200

    first commit

---
+++ python_data_types.py
@@ -0,0 +1,45 @@
+# An integer
+# The quotient of the division of m by n: m // n
+# The remainder of the division of m by n: m % n
+# The actual division: m / n
+# Read more: https://en.wikipedia.org/wiki/Euclidean_division
+my_integer = 3
+
+# A float
+# The integer part of a float x: int(x)
+my_float = 4.4
+
+# A boolean
+# Is either True or False
+my_boolean = True
+
+
+# A string of text
+# Characters between either single quotes or double quotes
+my_string = "flowers"
+
+# An ordered list of elements
+# Between square brackets
+# Elements are separated by comma
+# Elements can be of any valid data type
+# Same element can appear multiple times
+# Indexing starts from 0
+# Access the element in index i: my_list[i]
+# Number of elements in the list: len(my_list)
+my_list = ["blue", 66, False, 4.4, 4.4]
+
+# An unordered list of key-value pairs
+# Between curly brackets
+# Pair are separated by comma
+# Keys are between either single or double quotes
+# Keys and values are separated with colon
+# Values can be of any valid data type
+# Keys must be unique
+# 2 keys can have the same value
+# Access the value of a 'key': my_dict['key']
+# Number of elements in the dict: len(my_dict)
+my_dict = {
+    'name': "Pyynikki",
+    'age': 69,
+    'magic_number': 69
+}
